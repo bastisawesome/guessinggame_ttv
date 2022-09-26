32fa4a0129664c35dc72648f6c32f3fd7949ef8b
Giles Johnson <bastisawesomeltd@gmail.com>
2022-09-26 18:08:48 -0400

    Fix linting... Again.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,6 @@ from guessinggame_ttv.database import DatabaseManager
 import logging
 import pytest
 import sqlite3
-from typing import Union, Tuple
 
 logging.disable()
 
@@ -24,30 +23,30 @@ def dbmanagerfilled(dbmanager: DatabaseManager) -> DatabaseManager:
     conn = dbmanager._connection
 
     # Add two dummy categories
-    values: list[Union[Tuple[str, str], str]] = ['dummy1', 'dummy2']
+    cats = ['dummy1', 'dummy2']
     conn.executemany('INSERT INTO categories (name) VALUES  (?)',
-                     values)
+                     cats)
 
     # Add some dummy words
-    values = [('word1', 1), ('word2', 1), ('word3', 2), ('word4', 2)]
+    words = [('word1', 1), ('word2', 1), ('word3', 2), ('word4', 2)]
     conn.executemany('INSERT INTO wordlist (word, category_id) VALUES (?,?)',
-                     values)
+                     words)
 
     # Add some dummy users
-    values = [('MultiDarkSamuses', 3, 5), ('DummyUser', 4, 1)]
+    users = [('MultiDarkSamuses', 3, 5), ('DummyUser', 4, 1)]
     conn.executemany('INSERT INTO users (username, score, tokens)'
                      'VALUES (?, ?, ?)',
-                     values)
+                     users)
 
     # Add some dummy redeems
-    values = [('dummyredeem1', 1), ('dummyredeem2', 10)]
+    redeems = [('dummyredeem1', 1), ('dummyredeem2', 10)]
     conn.executemany('INSERT INTO redeems (name, cost) VALUES (?,?)',
-                     values)
+                     redeems)
 
     # Add some dummy metadata
-    values = [('dummymetastr', 'this is dummy meta'),
-              ('dummymetablob', '{"name": "test", "some_val": 1}')]
+    metadata = [('dummymetastr', 'this is dummy meta'),
+                ('dummymetablob', '{"name": "test", "some_val": 1}')]
     conn.executemany('INSERT INTO meta (name, data) VALUES (?,?)',
-                     values)
+                     metadata)
 
     return dbmanager
