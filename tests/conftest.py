72648c75636ddafb29d3faf3ed951b51c667a2cd
Giles Johnson <bastisawesomeltd@gmail.com>
2022-10-18 14:56:02 -0400

    Add tests.
    Resolves #13

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
-from guessinggame_ttv.database import DatabaseManager
+from guessinggame_ttv.database import (
+    DatabaseManager, UserNotFoundException, WordNotFoundException)
+from guessinggame_ttv.game import Game
 
 import logging
 import pytest
 import sqlite3
+
+from typing import Tuple
 
 logging.disable()
 
@@ -53,3 +57,121 @@ def dbmanagerfilled(dbmanager: DatabaseManager) -> DatabaseManager:
                      metadata)
 
     return dbmanager
+
+
+class DBManagerMock(DatabaseManager):
+    def __init__(self) -> None:
+        self.meta = {
+            'update_round': 'False',
+            'round_end': 'False'
+        }
+
+        self.wordlist = {
+            'test_cat': ['word1', 'word2']
+        }
+
+        self.users = {
+            'testuser': {
+                'score': 0,
+                'tokens': 0
+            }
+        }
+
+        self.redeems = {
+            'testRedeem': 0
+        }
+
+        self.categories = ['test_cat']
+
+    def get_meta(self, name: str) -> str | None:
+        return self.meta.get(name, None)
+
+    def set_meta(self, name: str, value: str) -> None:
+        self.meta[name] = value
+
+    def get_score(self, username: str) -> int:
+        user = self.users.get(username, None)
+
+        if user:
+            return user['score']
+
+        return 0
+
+    def reset_scores(self) -> None:
+        for user, data in self.users.items():
+            data['score'] = 0
+
+    def add_score(self, username: str, amount: int) -> None:
+        try:
+            self.users[username]['score'] += amount
+        except KeyError:
+            raise UserNotFoundException()
+
+    def get_category(self, word: str) -> str:
+        for cat, words in self.wordlist.items():
+            if word in words:
+                return cat
+        else:
+            return None
+
+    def get_tokens(self, username: str) -> int:
+        user = self.users.get(username, None)
+
+        if user:
+            return user['tokens']
+        else:
+            return 0
+
+    def set_tokens(self, username: str, amount: int) -> None:
+        user = self.users.get(username, None)
+
+        if user:
+            user['tokens'] = amount if amount > 0 else 0
+        else:
+            raise UserNotFoundException()
+
+    def add_tokens(self, username: str, amount: int) -> None:
+        user = self.users.get(username, None)
+
+        if user:
+            user['tokens'] -= amount
+        else:
+            raise UserNotFoundException()
+
+    def remove_tokens(self, username: str, amount: int) -> None:
+        user = self.users.get(username, None)
+
+        if not user:
+            raise UserNotFoundException()
+
+        num_toks = user['tokens']
+        final_amt = num_toks - amount
+
+        user['tokens'] = final_amt if final_amt > 0 else 0
+
+    def get_remaining_word_count(self) -> int:
+        return len(self.words_as_list())
+
+    def remove_word(self, word: str) -> None:
+        for words in self.wordlist.values():
+            if word in words:
+                words.remove(word)
+        else:
+            raise WordNotFoundException
+
+    def get_words(self) -> list[Tuple[str, str]]:
+        out_l: list[Tuple[str, str]] = []
+
+        for cat, words in self.wordlist.items():
+            for word in words:
+                out_l.append((word, cat))
+
+        return out_l
+
+    def words_as_list(self) -> list[str]:
+        return [word for words in self.wordlist.values() for word in words]
+
+
+@ pytest.fixture(scope='function')
+def gameobj() -> Game:
+    return Game(DBManagerMock())
---
+++ tests/test_game.py
@@ -0,0 +1,157 @@
+from guessinggame_ttv.game import Game
+from .conftest import DBManagerMock
+
+
+def test_game_initialisation() -> None:
+    game = Game(DBManagerMock())
+
+    assert game.logger is not None
+    assert game.databasemanager is not None
+    assert game.running is True
+    assert game.current_word is not None
+    assert game.current_point_value is not None
+    assert game.current_category is not None
+
+
+def test_game_init_round_end() -> None:
+    dbmanmock = DBManagerMock()
+    dbmanmock.meta['round_end'] = 'True'
+
+    game = Game(dbmanmock)
+
+    assert game.running is False
+    assert game.current_word == ''
+    assert game.current_point_value == ''
+    assert game.current_category == ''
+
+
+def test__load_previous_data_none() -> None:
+    dbmanmock = DBManagerMock()
+    game = Game(dbmanmock)
+
+    assert game._load_previous_data() is False
+
+    test_meta = {
+        'prev_word': 'testword',
+        'prev_cat': 'testcat',
+        'prev_points': '5',
+    }
+    dbmanmock.meta.update(test_meta)
+
+    assert game._load_previous_data() is True
+    assert game.current_word == test_meta['prev_word']
+    assert game.current_category == test_meta['prev_cat']
+    assert game.current_point_value == int(test_meta['prev_points'])
+
+
+def test_choose_new_word(gameobj: Game) -> None:
+    db: DBManagerMock = gameobj._databasemanager
+    catlist = db.wordlist.keys()
+    wordlist = db.words_as_list()
+
+    # Since it randomly selects a word, we can't test specific values.
+    # But we know the constraints are what is contained in the database,
+    # so we test against what is in the database.
+    assert gameobj.choose_new_word() is True
+    assert gameobj.current_word in wordlist
+    assert gameobj.current_category in catlist
+    assert gameobj.current_word not in db.wordlist
+
+    db.wordlist = {}
+
+    # Test that the function properly returns False on an empty wordlist
+    assert gameobj.choose_new_word() is False
+
+    # No testing against the current word/category/point value because the
+    # function does not change anything in this event, only returns false.
+
+
+def test_update_point_value(gameobj: Game) -> None:
+    # Prepare some extra wordlists to check against the various point values.
+    # The strings in here don't matter, but they must exist.
+    more_than_20 = {
+        'cat': [str(i) for i in range(24)]
+    }
+    more_than_11 = {
+        'cat': [str(i) for i in range(13)]
+    }
+
+    gameobj.update_point_value()
+    assert gameobj.current_point_value == 3
+
+    gameobj._databasemanager.wordlist = more_than_11
+    gameobj.update_point_value()
+    assert gameobj.current_point_value == 2
+
+    gameobj._databasemanager.wordlist = more_than_20
+    gameobj.update_point_value()
+    assert gameobj.current_point_value == 3
+
+
+def test_process_win_round_continue(gameobj: Game) -> None:
+    db: DBManagerMock = gameobj._databasemanager
+    # We can't know what the current word is, so we create a message with
+    # every single word in it.
+    message = ' '.join(db.words_as_list())
+    user = 'testuser'
+
+    results = gameobj.process(user, message)
+
+    assert results.success
+    assert results.word
+    assert results.score
+    assert results.words_remaining == 1
+
+
+def test_process_win_round_end(gameobj: Game) -> None:
+    db: DBManagerMock = gameobj._databasemanager
+    db.wordlist = {'testcat': ['word1']}
+    message = 'word1'
+    user = 'testuser'
+
+    results = gameobj.process(user, message)
+
+    assert results.success
+    assert results.word
+    assert results.score
+    assert results.words_remaining == 0
+
+
+def test_process_lose(gameobj: Game) -> None:
+    results = gameobj.process('testuser', 'no words here')
+
+    assert not results.success
+    assert results.word is None
+    assert results.score is None
+    assert results.words_remaining == 2
+
+
+def test__end_round(gameobj: Game) -> None:
+    db: DBManagerMock = gameobj._databasemanager
+    db.users['testuser']['score'] = 5
+    db.users['testuser2'] = {
+        'score': 5,
+        'tokens': 0
+    }
+    db.users['testuser3'] = {
+        'score': 4,
+        'tokens': 0
+    }
+    db.users['testuser4'] = {
+        'score': 3,
+        'tokens': 0
+    }
+
+    gameobj._end_round()
+
+    assert db.users['testuser']['score'] == 0
+    assert db.users['testuser']['tokens'] == 3
+
+    assert db.users['testuser2']['score'] == 0
+    assert db.users['testuser2']['tokens'] == 3
+
+    assert db.users['testuser3']['score'] == 0
+    assert db.users['testuser3']['tokens'] == 2
+
+    assert db.users['testuser']['score'] == 0
+    assert db.users['testuser']['tokens'] == 1
