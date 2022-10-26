b937870ec86810f6d9a7b595e4538be60a712638
Giles Johnson <bastisawesomeltd@gmail.com>
2022-10-26 16:15:52 -0400

    Update game module tests.

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,9 +1,7 @@
-'''
-FIXME: Add tests for words inside other words, should fail
-'''
-
 from guessinggame_ttv.game import Game
 from .conftest import DBManagerMock
+
+import pytest
 
 
 def test_game_initialisation() -> None:
@@ -17,16 +15,71 @@ def test_game_initialisation() -> None:
     assert game.category is not None
 
 
+def test_game_initialisation_no_meta() -> None:
+    dbmanmock = DBManagerMock()
+    dbmanmock.meta = {}
+
+    game = Game(dbmanmock)
+
+    assert game.running is True
+    assert game.word is not None
+    assert game.point_value is not None
+    assert game.category is not None
+
+
+def test_game_initialisation_distribute_points() -> None:
+    dbmanmock = DBManagerMock()
+    dbmanmock.meta = {
+        'round_end': 'True',
+        'distribute_points': 'True'
+    }
+
+    game = Game(dbmanmock)
+
+    assert game.running is False
+    assert not game.word
+    assert not game.point_value
+    assert not game.category
+
+
 def test_game_init_round_end() -> None:
     dbmanmock = DBManagerMock()
     dbmanmock.meta['round_end'] = 'True'
 
     game = Game(dbmanmock)
 
-    assert game.running is False
-    assert game.word == ''
-    assert game.point_value == 0
-    assert game.category == ''
+    assert game._running is False
+    assert game._current_word == ''
+    assert game._current_point_value == 0
+    assert game._current_category == ''
+
+
+def test_game_init_loaded_data_update_round(dbmanmock: DBManagerMock) -> None:
+    dbmanmock.meta = {
+        'update_round': 'True',
+        'cur_word': 'waffle',
+        'cur_points': '10',
+        'cur_cat': 'food'
+    }
+
+    game = Game(dbmanmock)
+
+    assert game._running is True
+    assert game._current_word == 'waffle'
+    assert game._current_point_value != 10
+    assert game._current_category == 'food'
+
+
+def test_game_init_no_data(dbmanmock: DBManagerMock) -> None:
+    dbmanmock.wordlist = {}
+    dbmanmock.meta = {}
+
+    game = Game(dbmanmock)
+
+    assert game._running is False
+    assert not game._current_word
+    assert not game._current_point_value
+    assert not game._current_category
 
 
 def test__load_previous_data_none() -> None:
@@ -39,8 +92,8 @@ def test__load_previous_data_none() -> None:
 def test__load_previous_data() -> None:
     dbmanmock = DBManagerMock()
     test_meta = {
-        'cur_word': 'testword',
-        'cur_cat': 'testcat',
+        'cur_word': 'word1',
+        'cur_cat': 'test_cat',
         'cur_points': '5',
     }
     dbmanmock.meta.update(test_meta)
@@ -50,6 +103,7 @@ def test__load_previous_data() -> None:
     assert game.word == test_meta['cur_word']
     assert game.category == test_meta['cur_cat']
     assert game.point_value == int(test_meta['cur_points'])
+    assert dbmanmock.meta['update_round'] == 'False'
 
 
 def test_choose_new_word(gameobj: Game) -> None:
@@ -98,7 +152,7 @@ def test_update_point_value(gameobj: Game) -> None:
 
 def test_process_win_round_continue(gameobj: Game) -> None:
     message = f'test message that has {gameobj.word} in it.'
-    user = 'testuser'
+    user = 'newuser'
 
     results = gameobj.process(user, message)
 
@@ -131,6 +185,20 @@ def test_process_lose(gameobj: Game) -> None:
     assert results.words_remaining == 2
 
 
+@pytest.mark.parametrize('word_to_guess,word_guessed,expected_result',
+                         [
+                             ['test', 'testing', False],
+                             ['prime', 'PrImE', True],
+                             ['win', 'WinNer', False],
+                             ['nachos', 'Nachos are a fantastic food.', True]
+                         ])
+def test__word_in_message(gameobj: Game, word_to_guess: str,
+                          word_guessed: str, expected_result: bool) -> None:
+    gameobj._current_word = word_to_guess
+
+    assert gameobj._word_in_message(word_guessed) == expected_result
+
+
 def test_end_round(gameobj: Game) -> None:
     db: DBManagerMock = gameobj._databasemanager
     db.users['testuser']['score'] = 5
@@ -160,6 +228,15 @@ def test_end_round(gameobj: Game) -> None:
 
     assert db.users['testuser4']['score'] == 0
     assert db.users['testuser4']['tokens'] == 1
+
+
+def test__distribute_points_no_users(gameobj: Game) -> None:
+    dbmanmock: DBManagerMock = gameobj._databasemanager
+    dbmanmock.users = {}
+
+    gameobj._distribute_points([])
+
+    assert not dbmanmock.users
 
 
 def test_teardown_running(gameobj: Game) -> None:
