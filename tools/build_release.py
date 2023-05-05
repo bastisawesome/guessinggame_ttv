1b902e15789eb1b5abd1cf1a8ed86f1ee71dd27a
Giles Johnson <bastisawesomeltd@gmail.com>
2023-05-05 19:07:44 -0400

    Fingers crossed, this implements MacOS and Linux build support.

--- tools/build_release.py
+++ tools/build_release.py
@@ -2,14 +2,15 @@ import os
 import sys
 import subprocess
 import pathlib
-import shutil
 import zipfile
 import tarfile
-
+from guessinggame_ttv._version import version
 
 # Annoying and stupid globals to ease life in per-platform archiving.
 DIST_PATH = pathlib.Path('dist')
 TEMPLATES_PATH = pathlib.Path('templates')
+NIX_BINARY_NAME = 'guessinggame_ttv'
+WINDOWS_BINARY_NAME = 'guessinggame_ttv.exe'
 
 
 def check_virtual_env() -> bool:
@@ -38,7 +39,52 @@ You can run the script in one of two ways:
         'guessinggame_ttv.spec'],
         check=True)
 
-    print('Building done, preparing assets for packaging.')
+    print('Building done, archiving application')
+
+    if sys.platform.startswith('linux'):
+        build_linux()
+    elif sys.platform.startswith('darwin'):
+        build_macos()
+    elif sys.platform.startswith('win32'):
+        build_windows()
+    else:
+        print('Unsupported operating system. Feel free to make a pull request'
+              'to add more supported platforms.')
+        sys.exit(1)
+
+    print('Archive created in `dist`')
+
+
+def build_linux() -> None:
+    write_tarball(NIX_BINARY_NAME)
+
+
+def build_macos() -> None:
+    # In case of future differences, this build function will remain separate
+    # from the Linux build function, despite both doing the same things.
+    write_tarball(NIX_BINARY_NAME)
+
+
+def write_tarball(bin_name: str) -> None:
+    # 1. Add the executable to the tarball
+    # 2. Add templates directory to the tarball
+    # 3. Add readme to the tarball
+    executable_path = DIST_PATH / bin_name
+    readme_path = pathlib.Path('README.md')
+
+    with tarfile.open(
+            name=DIST_PATH / f'guessinggame_ttv-{version}.tar.gz',
+            mode='w:gz') as out_arc:
+        out_arc.add(executable_path,
+                    arcname=f'guessinggame_ttv/{bin_name}')
+        out_arc.add(readme_path,
+                    arcname=f'guessinggame_ttv/{readme_path}')
+        out_arc.add(TEMPLATES_PATH,
+                    arcname=f'guessinggame_ttv/{TEMPLATES_PATH}')
+
+
+def build_windows():
+    raise NotImplementedError()
 
 
 if __name__ == '__main__':
